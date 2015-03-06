188b5639fddb6eb211b8aea6a6df6e6348db63cf
Dumbo52 <dumbofiftytwo@gmail.com>
2015-03-05 23:12:13 -0500

    Fix /tpunfriend and /tpunblock; formatting.

--- src/nu/nerd/tpcontrol/User.java
+++ src/nu/nerd/tpcontrol/User.java
@@ -4,27 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 public class User {
     private TPControl plugin;
-    
+
     public String username;
-    
+
     //Ask-mode stuff
     String last_applicant;
     long last_applicant_time;
-    
+
     private File prefs_path;
     private YamlConfiguration yaml;
     private boolean dirty = false;
     private Location lastLocation;
-    
+
     public User (TPControl instance, Player p) {
         //player = instance.getServer().getPlayer(u);
         plugin = instance;
@@ -44,7 +44,7 @@ public class User {
         if(!dirty) {
             return;
         }
-        
+
         try {
             yaml.save(prefs_path);
         } catch (IOException e) {
@@ -53,68 +53,68 @@ public class User {
         }
         dirty = false;
     }
-    
+
     //
     // Last Location handling.
     //
     public void setLastLocation(Location last) {
         this.lastLocation = last;
     }
-    
+
     public Location getLastLocation() {
         return this.lastLocation;
     }
-    
+
     //
     // Black/whitelist handling.
     //
-    
+
     private List<String> getList(String name) {
         return yaml.getStringList(name);
     }
-    
+
     private boolean addToList(String name, String username) {
         List<String> l = getList(name);
 
-		String uuid = null;
-		Player perp = plugin.getPlayer(username);
-		if (perp != null)
-			uuid = perp.getUniqueId().toString();
-		else
-			uuid = Bukkit.getOfflinePlayer(username).getUniqueId().toString();
+        String uuid = null;
+        Player perp = plugin.getPlayer(username);
+        if (perp != null)
+            uuid = perp.getUniqueId().toString();
+        else
+            uuid = Bukkit.getOfflinePlayer(username).getUniqueId().toString();
 
         if(l.contains(uuid)) {
             return false;
         }
-        
+
         l.add(uuid);
         yaml.set(name,l);
         dirty = true;
         return true;
     }
-    
-    private boolean delFromList (String name, String username) {
+
+    private boolean delFromList(String name, String username) {
         List<String> l = getList(name);
 
-		String uuid = null;
-		Player perp = plugin.getPlayer(username);
-		if (perp != null)
-			uuid = perp.getUniqueId().toString();
-		else
-			uuid = Bukkit.getOfflinePlayer(username).getUniqueId().toString();
+        String uuid = null;
+        Player perp = plugin.getPlayer(username);
+        if (perp != null)
+            uuid = perp.getUniqueId().toString();
+        else
+            uuid = Bukkit.getOfflinePlayer(username).getUniqueId().toString();
 
-        if(l.contains(uuid)) {
+        if(!l.contains(uuid)) {
             return false;
         }
-        
+
         l.remove(uuid);
         yaml.set(name,l);
         dirty = true;
         return true;
     }
-    
+
     //Friends...
-    
+
     public boolean addFriend(String username) {
         return addToList("friends", username);
     }
@@ -124,9 +124,9 @@ public class User {
     public List<String> getFriends() {
         return getList("friends");
     }
-    
+
     //Blocked...
-    
+
     public boolean addBlocked(String username) {
         return addToList("blocked", username);
     }
@@ -136,13 +136,13 @@ public class User {
     public List<String> getBlocked() {
         return getList("blocked");
     }
-    
+
     //
     // Player mode (allow|ask|deny)
     //
-    
+
     // Set this player's absolute mode
-    
+
     public void setMode(String mode) {
         yaml.set("mode", mode);
         dirty = true;
@@ -150,7 +150,7 @@ public class User {
     public String getMode() {
         return yaml.getString("mode");
     }
-    
+
     // Get the calculated mode for an applicant teleporting
     // to us - i.e., take into account black/whitelisting.
     public String getCalculatedMode(Player applicant) {
@@ -163,10 +163,10 @@ public class User {
         if(getBlocked().contains(uuid)) {
             relation = "blocked";
         }
-        
+
         return plugin.config.getCalculatedMode(mode, relation);
     }
-    
+
     //For 'ask' mode:
     public void lodgeRequest(Player applicant) {
         String applicant_username = ChatColor.stripColor(applicant.getName()).toLowerCase();
